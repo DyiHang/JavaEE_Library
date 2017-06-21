2b759305a1a36033f79b59bc7f4c9d6ec68d15e5
mingfei <mingfei.net@gmail.com>
2017-06-21 12:01:58 +0800

    Update

--- src/main/java/demo/servlet/UserAction.java
+++ src/main/java/demo/servlet/UserAction.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -40,6 +41,11 @@ public class UserAction extends HttpServlet {
 
         if ("logout".equals(action)) {
             logout(req, resp);
+            return;
+        }
+
+        if ("isUsernameExist".equals(action)) {
+            isUsernameExist(req, resp);
             return;
         }
 
@@ -94,31 +100,74 @@ public class UserAction extends HttpServlet {
         }
     }
 
-    private void register(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username").trim();
-        String password = req.getParameter("password");
 
+    private void isUsernameExist(HttpServletRequest req, HttpServletResponse response) {
+        String username = req.getParameter("username").trim();
+        System.out.println(username);
         Connection connection = Db.getConnection();
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
         String sql = "SELECT * FROM javaee_library.user WHERE username = ?";
 
+        response.setContentType("text/html charset=UTF-8");
+        Writer writer = null;
         try {
+            writer = response.getWriter();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (connection != null) {
+                preparedStatement = connection.prepareStatement(sql);
+            } else {
+                // TODO: 6/21/17
+
+            }
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                writer.write("true");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            Db.close(resultSet, preparedStatement, connection);
+        }
+
+        try {
+            writer.write("false");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void register(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username").trim();
+        String password = req.getParameter("password");
+
+        Connection connection = Db.getConnection();
+        PreparedStatement preparedStatement = null;
+
+//        if (isUsernameExist(req, resp)) {
+//            req.setAttribute("message", "用户名存在");
+//            req.getRequestDispatcher("register.jsp").forward(req, resp);
+//            return;
+//        }
+
+        try {
+            String sql = "INSERT INTO javaee_library.user(username, password) VALUES(?, ?)";
+
             if (connection != null) {
                 preparedStatement = connection.prepareStatement(sql);
             } else {
                 Error.showError(req, resp);
                 return;
             }
-            preparedStatement.setString(1, username);
-            resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                req.setAttribute("message", "用户名存在");
-                return;
-            }
 
-            sql = "INSERT INTO javaee_library.user(username, password) VALUES(?, ?)";
             preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
@@ -128,7 +177,7 @@ public class UserAction extends HttpServlet {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            Db.close(resultSet, preparedStatement, connection);
+            Db.close(null, preparedStatement, connection);
         }
     }
 
--- src/main/webapp/register.jsp
+++ src/main/webapp/register.jsp
@@ -7,13 +7,32 @@
 <html>
 <head>
     <title>注册</title>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script>
+        $(function () {
+            $('#username').blur(function () {
+                $.ajax({
+                    url: 'user',
+                    data: {'action': 'isUsernameExist', 'username': $('#username').val()},
+                    success: function (data) {
+                        console.log(data);
+                        if (data == 'true') {
+                            $('span').text('用户名已经存在');
+                        } else {
+                            $('span').text('用户名可以使用');
+                        }
+                    }
+                });
+            });
+        });
+    </script>
 </head>
 <body>
 <h1>注册</h1>
 <hr>
 <form action="user" method="post">
     <input type="hidden" name="action" value="register">
-    <input type="text" name="username" placeholder="用户名"><br>
+    <input id="username" type="text" name="username" placeholder="用户名"><span></span><br>
     <input type="password" name="password" placeholder="密码"><br>
     <input type="submit" value="注册">
 </form>
